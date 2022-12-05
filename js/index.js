ded4ab6d6dad1d3038620a9cf2754ff6ecfecd4d
aditya phasu <adityaphasu919@gmail.com>
2022-12-05 19:36:01 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$('.fa-earth-asia').hover(
+  function() {
+    $(this).addClass('fa-spin');
+    $(this).css("--fa-animation-duration", "15s");
+  },
+  function() {
+    $(this).removeClass("fa-spin");
+  }
+);
+
+$('.fa-wheat-awn').hover(
+  function() {
+    $(this).addClass('fa-shake');
+    $(this).css("--fa-animation-duration", "5s");
+  },
+  function() {
+    $(this).removeClass("fa-shake");
+  }
+);
+
+$('.fa-truck-fast').hover(
+  function() {
+    $(this).addClass('fa-beat-fade');
+  },
+  function() {
+    $(this).removeClass("fa-beat-fade");
+  }
+);
